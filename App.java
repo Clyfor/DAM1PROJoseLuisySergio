6a1d126bb7e40c278772ba0c0fe8be7cef23a359
Sergio <sergio_22-09-92@hotmail.com>
2025-03-25 16:51:18 +0100

    Se ha rellenado el main (App)

--- App.java
+++ App.java
@@ -1,3 +1,20 @@
-public class App {
+import java.io.IOException;
 
+import GestionConfiguracion.ArchivoConfiguracion;
+import GestionDirectorios.ArchivoDirectorios;
+
+public class App {
+    public static void main(String[] args) throws IOException{
+        if(!ArchivoConfiguracion.configuracionExiste()){
+            try {
+                ArchivoConfiguracion.crearArchivoConfig();
+                ArchivoDirectorios.crearDirectorios();
+            } catch (IOException e) {
+                System.out.println("Error al crear el archivo de configuración "+
+                "\nMensaje de la excepcion: "+e.getMessage());
+            }
+        } else {
+            System.out.println("El archivo de configuración ya existe!");
+        }
+    }
 }
