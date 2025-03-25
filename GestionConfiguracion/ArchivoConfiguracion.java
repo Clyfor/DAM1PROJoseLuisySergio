33b7a9d8c4b72ff6abd96487dd8ea28dc3c84c6b
José Luis <jlmorenot03@iesalbarregas.es>
2025-03-25 16:45:31 +0100

    Completar Archivo de configuracion

--- GestionConfiguracion/ArchivoConfiguracion.java
+++ GestionConfiguracion/ArchivoConfiguracion.java
@@ -1,5 +1,22 @@
 package GestionConfiguracion;
 
-public class ArchivoConfiguracion {
+import java.io.File;
+import java.io.IOException;
 
+public class ArchivoConfiguracion {
+    private static final String nombreArchivo = "configuracion.txt";
+
+    public static boolean configuracionExiste() {
+        File archivo = new File(nombreArchivo);
+        return archivo.exists();
+    }
+
+    public static void crearArchivoConfig() throws IOException {
+        File archivo2 = new File(nombreArchivo);
+        if (archivo2.createNewFile()) {
+            System.out.println("Archivo de configuracion creado");
+        } else {
+            System.out.println("No se ha podido crear el archivo de configuracion");
+        }
+    }
 }
