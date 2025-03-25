a9d2a68b6aa577db53fa18c954a3134986b8bc75
Sergio <sergio_22-09-92@hotmail.com>
2025-03-25 16:39:12 +0100

    Se ha rellenado la clase ArchivoDirectorios

--- GestionDirectorios/ArchivoDirectorios.java
+++ GestionDirectorios/ArchivoDirectorios.java
@@ -1,5 +1,36 @@
 package GestionDirectorios;
 
+import java.io.File;
+
+//Clase ArchivoDirectorios
 public class ArchivoDirectorios {
 
+    //Creamos un array de Strings estatico con los nombres de nuestros directorios
+    private static final String[] Directorios = {"jugadores", "escenarios", "partidas" };
+
+    /* 
+     * Creamos la funcion que recorre nuestro array de Strings(Directorios) y los va creando
+    */
+    public static void crearDirectorios(){
+
+        for (String nombreDirectorio : Directorios){
+
+            File directorio = new File(nombreDirectorio);
+
+            //Si el directorio no existe...
+            if(!directorio.exists()){
+
+                //Comando para intentar crear la carpeta (en caso de que no exista)
+                if(directorio.mkdir()){
+
+                    System.out.println("Directorio "+nombreDirectorio+" creado");
+
+                }else{
+                    //La carpeta ya existe
+                    System.out.println("No se pudo crear el directorio "+nombreDirectorio);
+
+                }
+            }
+        }
+    }
 }
