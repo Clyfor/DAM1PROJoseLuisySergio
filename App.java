4829fd31c36fdc9cd82086396dd3dc230d86dcd0
José Luis <jlmorenot03@iesalbarregas.es>
2025-03-26 16:20:19 +0100

    Se han añadido autor, version etc

--- App.java
+++ App.java
@@ -1,8 +1,15 @@
 import java.io.IOException;
-
 import GestionConfiguracion.ArchivoConfiguracion;
 import GestionDirectorios.ArchivoDirectorios;
 
+/**
+ * Clase principal de la aplicación.
+ * 
+ * @author Jose Luis Moreno Torés y Sergio Ramírez Pla
+ * @version GPL v_3
+ * @date 2023-10-01
+ * @description Esta clase se encarga de crear el archivo de configuración y los directorios necesarios para la aplicación.
+ */
 public class App {
     public static void main(String[] args) throws IOException {
         try {
